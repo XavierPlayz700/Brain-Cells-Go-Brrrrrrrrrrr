a2b644ae142a84f343b7b2042a9428ff9e57ff84
Kokichi <75687135+XavierPlayz700@users.noreply.github.com>
2021-09-16 19:48:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Troll Quiz
+
+I want waffle fries and coffee. Have fun suffering with the Rick Roll *sip* because I am evil.
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz, music, pic;
+
+function preload () {
+  // Suffer with Music because Yes
+  music = loadSound("Rickroll.mp3");
+  pic = loadImage("XaviDab.png");
+}
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+
+  music.loop();
+  music.setVolume(0.5);
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+
+  imageMode(CENTER);
+  image(pic, width * 0.8, height * 0.34, 100, 130);
+}
